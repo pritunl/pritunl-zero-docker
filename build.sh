da94e73432ef552189ac1f8337097ad2775b425e
Zachary Huff <zach.huff.386@gmail.com>
2019-09-27 04:07:10 -0400

    Update docker build

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ sudo docker pull oraclelinux:7
 sudo docker build --rm -t pritunl-zero .
 
 export version="1.0.1317.75"
-sudo docker tag pritunl-zero:latest pritunl/pritunl-zero:"$version"
-sudo docker tag pritunl-zero:latest pritunl/pritunl-zero:latest
-sudo docker push pritunl/pritunl-zero:"$version"
-sudo docker push pritunl/pritunl-zero:latest
+sudo docker tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:"$version"
+sudo docker tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:latest
+sudo docker push docker.io/pritunl/pritunl-zero:"$version"
+sudo docker push docker.io/pritunl/pritunl-zero:latest
 echo $version
