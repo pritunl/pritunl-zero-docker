d03a8504e169c361fa2e3edaa41f5c3a32f8fb5c
Zachary Huff <zach.huff.386@gmail.com>
2019-09-27 04:12:22 -0400

    Update build

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ sudo docker build --rm -t pritunl-zero .
 export version="1.0.1403.26"
 sudo docker tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:"$version"
 sudo docker tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:latest
-sudo docker push docker.io/pritunl/pritunl-zero:"$version"
-sudo docker push docker.io/pritunl/pritunl-zero:latest
+sudo docker push --authfile=/etc/containers/auth.json docker.io/pritunl/pritunl-zero:"$version"
+sudo docker push --authfile=/etc/containers/auth.json docker.io/pritunl/pritunl-zero:latest
 echo $version
