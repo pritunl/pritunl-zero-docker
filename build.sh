2e070b54feec0ed7f32eec90c048e6d449775649
Zachary Huff <zach.huff.386@gmail.com>
2019-09-28 00:33:26 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo docker pull oraclelinux:7
-sudo docker build --rm -t pritunl-zero .
+sudo docker build --rm --no-cache -t pritunl-zero .
 
 export version="1.0.1403.26"
 sudo docker tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:"$version"
