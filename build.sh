10009b2a15b0f755fc6a39efc2478cde384d333c
Zachary Huff <zach.huff.386@gmail.com>
2019-10-04 17:02:27 -0400

    Add github packages to build

--- build.sh
+++ build.sh
@@ -5,8 +5,15 @@ sudo docker pull oraclelinux:7
 sudo docker build --rm --no-cache -t pritunl-zero .
 
 export version="1.0.1406.45"
+
 sudo docker tag pritunl-zero:latest iad.ocir.io/pritunl8472/pritunl-zero:"$version"
 sudo docker tag pritunl-zero:latest iad.ocir.io/pritunl8472/pritunl-zero:latest
 sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-zero:"$version"
 sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-zero:latest
+
+sudo docker tag pritunl-zero:latest docker.pkg.github.com/pritunl/pritunl-zero/pritunl-zero:"$version"
+sudo docker tag pritunl-zero:latest docker.pkg.github.com/pritunl/pritunl-zero/pritunl-zero:latest
+sudo docker push --authfile=/etc/containers/auth.json docker.pkg.github.com/pritunl/pritunl-zero/pritunl-zero:"$version"
+sudo docker push --authfile=/etc/containers/auth.json docker.pkg.github.com/pritunl/pritunl-zero/pritunl-zero:latest
+
 echo $version
