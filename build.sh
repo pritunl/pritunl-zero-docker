7ddea19e68b768280495ba303bba61342bf79e83
Zachary Huff <zach.huff.386@gmail.com>
2019-12-31 03:30:47 -0500

    Fix docker build

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ sudo docker tag pritunl-zero:latest iad.ocir.io/pritunl8472/pritunl-zero:latest
 sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-zero:"$version"
 sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-zero:latest
 
+sudo docker build --rm --no-cache --format docker -t pritunl-zero .
+
 sudo docker tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:"$version"
 sudo docker tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:latest
 sudo docker push --authfile=/etc/containers/auth.json docker.io/pritunl/pritunl-zero:"$version"
