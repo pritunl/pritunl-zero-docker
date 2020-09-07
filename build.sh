468c9df1f9f93e8447b3fffed937679478a633a7
Zachary Huff <zach.huff.386@gmail.com>
2020-09-07 12:53:44 -0400

    Update version 1.0.1741.88

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 sudo docker pull oraclelinux:7
 sudo docker build --rm --no-cache -t pritunl-zero .
 
-export version="1.0.1696.91"
+export version="1.0.1741.88"
 
 sudo docker tag pritunl-zero:latest iad.ocir.io/pritunl8472/pritunl-zero:"$version"
 sudo docker tag pritunl-zero:latest iad.ocir.io/pritunl8472/pritunl-zero:latest
