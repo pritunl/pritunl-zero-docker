64c9f1fb5aa74c0376b67356938773404610c380
Zachary Huff <zach.huff.386@gmail.com>
2022-01-26 20:53:26 -0500

    Update build

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
 
-sudo docker pull oraclelinux:7
-sudo docker build --rm --no-cache -t pritunl-zero .
+sudo podman pull oraclelinux:7
+sudo podman build --rm --no-cache -t pritunl-zero .
 
 export version="1.0.2250.3"
 
-sudo docker tag pritunl-zero:latest iad.ocir.io/pritunl8472/pritunl-zero:"$version"
-sudo docker tag pritunl-zero:latest iad.ocir.io/pritunl8472/pritunl-zero:latest
-sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-zero:"$version"
-sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-zero:latest
+sudo podman tag pritunl-zero:latest iad.ocir.io/pritunl8472/pritunl-zero:"$version"
+sudo podman tag pritunl-zero:latest iad.ocir.io/pritunl8472/pritunl-zero:latest
+sudo podman push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-zero:"$version"
+sudo podman push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-zero:latest
 
-sudo docker build --rm --no-cache --format docker -t pritunl-zero .
+sudo podman build --rm --no-cache --format docker -t pritunl-zero .
 
-sudo docker tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:"$version"
-sudo docker tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:latest
-sudo docker push --authfile=/etc/containers/auth.json docker.io/pritunl/pritunl-zero:"$version"
-sudo docker push --authfile=/etc/containers/auth.json docker.io/pritunl/pritunl-zero:latest
+sudo podman tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:"$version"
+sudo podman tag pritunl-zero:latest docker.io/pritunl/pritunl-zero:latest
+sudo podman push --authfile=/etc/containers/auth.json docker.io/pritunl/pritunl-zero:"$version"
+sudo podman push --authfile=/etc/containers/auth.json docker.io/pritunl/pritunl-zero:latest
 
 echo $version
